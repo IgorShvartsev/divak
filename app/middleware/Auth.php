e8a54c5207102bd3eaa155777f14fd080496af95
IgorShvartsev <igor.shvartsev@gmail.com>
2018-12-25 23:50:59 +0200

    update

--- app/middleware/Auth.php
+++ app/middleware/Auth.php
@@ -3,9 +3,10 @@ namespace Middleware;
 
 class Auth
 {
-	public function handle($response, \Closure $next)
-	{
-		$response =  $next($response);
-		return $response;
-	}
+    public function handle($response, \Closure $next)
+    {
+        $response =  $next($response);
+        
+        return $response;
+    }
 }
--- public/index.php
+++ public/index.php
@@ -5,13 +5,12 @@ define('STORAGE_PATH', dirname(__FILE__) . '/../storage');
 require_once(dirname(__FILE__) . '/../vendor/autoload.php');
 
 $dirs = [
-	dirname(__FILE__) . '/../core',
-	APP_PATH,
-	APP_PATH . '/controllers',
-	APP_PATH . '/core',
-	APP_PATH . '/models',
-	APP_PATH . '/library',
-	APP_PATH . '/helpers'
+    APP_PATH,
+    APP_PATH . '/controllers',
+    APP_PATH . '/core',
+    APP_PATH . '/models',
+    APP_PATH . '/library',
+    APP_PATH . '/helpers'
 ];
 
 \Loader::addDirectories($dirs);
